29145867f5d28d637f9d4363ccad941b8fed1cb9
Anirudha Jadhav <anirudhasj441@gmail.com>
2023-04-11 19:47:49 +0530

    initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,379 @@
+<template>
+  <main>
+    <div class="top-row"></div>
+    <div class="header row">
+      <div class="col-7 pt-5 ps-5 name-container">
+        <h1 class="text-name m-0">Anirudha</h1>
+        <h1 class="text-name m-0">Jadhav</h1>
+        <h3 class="m-0 mt-3">Software Developer</h3>
+      </div>
+      <div class="col-5 pt-5 info-container d-flex justify-content-end">
+        <ul>
+          <li>
+            <p class="m-0 me-2">
+              +91 9324141259
+            </p>
+            <div class="info-icon px-3">
+              <div id="first-icon"></div>
+              <i class="bi bi-telephone-fill"></i>
+            </div>
+          </li>
+          <div class="divider"></div>
+          <li>
+            <a href="mailto:anirudhasj441@gmail.com" class="m-0 me-2">
+              anirudhasj441@gmail.com
+            </a>
+            <div class="info-icon px-3">
+              <i class="bi bi-envelope-fill"></i>
+            </div>
+          </li>
+          <div class="divider"></div>
+          <li>
+            <a href="https://www.anirudhajadhav.com" class="m-0 me-2">
+              www.anirudhajadhav.com
+            </a>
+            <div class="info-icon px-3">
+              <i class="bi bi-globe"></i>
+            </div>
+          </li>
+          <div class="divider"></div>
+          <li>
+            <a href="https://github.com/anirudhasj441" class="m-0 me-2">
+              anirudhasj441
+            </a>
+            <div class="info-icon px-3">
+              <i class="bi bi-github"></i>
+            </div>
+          </li>
+          <div class="divider"></div>
+          <li>
+            <p class="m-0 me-2">
+              Mumbai, Maharashtra, India
+            </p>
+            <div class="info-icon px-3">
+              <i class="bi bi-geo-alt-fill"></i>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="divider"></div>
+
+    <div class="body p-4">
+      <div class="row">
+        <div class="col-3">
+          <div class="photo">
+            <img src="../assets/photo_new.jpg" alt="">
+          </div>
+          <div class="personal-info p-4 pt-2">
+            <!-- <h3 style="letter-spacing: 3px;">EDUCATION</h3>
+            <h5 class="mt-3">B.Sc (Computer Science)</h5>
+            <h6 class="">Mumbai University</h6>
+            <h6>2018 - 2021</h6>
+            <h5 class="mt-3">H.S.C (Science)</h5>
+            <h6 class="">Maharashtra Board</h6>
+            <h6>2016 - 2018</h6>
+            <div class="divider my-4"></div> -->
+            <h3 style="letter-spacing: 3px;">SKILLS</h3>
+            <ul class="skills ps-3">
+              <li class="my-2">
+                <div class="skill-icon p-1">
+                  <img src="../assets/python-5.svg" width="18">
+                </div>
+                <p class="m-0">Python</p>
+              </li>
+              <li class="my-2">
+                <div class="skill-icon p-1">
+                  <img src="../assets/django.svg" width="18">
+                </div>
+                <p class="m-0">Django</p>
+              </li>
+              <li class="my-2">
+                <div class="skill-icon p-1">
+                  <img src="../assets/flask.svg" width="18">
+                </div>
+                <p class="m-0">Flask</p>
+              </li>
+              <li class="my-2">
+                <div class="skill-icon p-1">
+                  <img src="../assets/php-1.svg" width="18">
+                </div>
+                <p class="m-0">PHP</p>
+              </li>
+              <li class="my-2">
+                <div class="skill-icon p-1">
+                  <img src="../assets/html-1.svg" width="18">
+                </div>
+                <p class="m-0">HTML</p>
+              </li>
+              <li class="my-2">
+                <div class="skill-icon p-1">
+                  <img src="../assets/css-3.svg" width="18">
+                </div>
+                <p class="m-0">CSS</p>
+              </li>
+              <li class="my-2">
+                <div class="skill-icon p-1">
+                  <img src="../assets/javascript-1.svg" width="18">
+                </div>
+                <p class="m-0">JavaScript</p>
+              </li>
+              <li class="my-2">
+                <div class="skill-icon p-1">
+                  <img src="../assets/logo.svg" width="18">
+                </div>
+                <p class="m-0">Vue JS</p>
+              </li>
+              <li class="my-2">
+                <div class="skill-icon p-1">
+                  <img src="../assets/bootstrap-5-1.svg" width="18">
+                </div>
+                <p class="m-0">Bootstrap</p>
+              </li>
+              <li class="my-2">
+                <div class="skill-icon p-1">
+                  <img src="../assets/quasar.png" width="18">
+                </div>
+                <p class="m-0">Quasar</p>
+              </li>
+              <li class="my-2">
+                <div class="skill-icon p-1">
+                  <img src="../assets/git-icon.svg" width="18">
+                </div>
+                <p class="m-0">Git</p>
+              </li>
+              <li class="my-2">
+                <div class="skill-icon p-1">
+                  <img src="../assets/linux-tux-2.svg" width="18">
+                </div>
+                <p class="m-0">Linux</p>
+              </li>
+              <li class="my-2">
+                <div class="skill-icon p-1">
+                  <img src="../assets/aws-ec2.svg" width="18">
+                </div>
+                <p class="m-0">AWS EC2</p>
+              </li>
+            </ul>
+            <div class="divider my-4"></div>
+            <h3 style="letter-spacing: 3px;">PERSONNEL SKILLS</h3>
+            <ul class="">
+              <li>Resilient</li>
+              <li>Observant</li>
+              <li>Team player</li>
+              <li>Optimistic</li>
+              <li>Interactive</li>
+            </ul>
+            <div class="page-break"></div>
+            <h3 style="letter-spacing: 3px;" class="mt-5">INTEREST</h3>
+            <ul class="">
+              <li>Web Application</li>
+              <li>Scripting</li>
+              <li>Linux Customization</li>
+            </ul>
+            <div class="divider my-4"></div>
+            <h3 style="letter-spacing: 3px;">COURSES</h3>
+            <ul class="">
+              <li>Python(Live Wire, Chembur)</li>
+              <li>MS-CIT (Anubhav, Chembur)</li>
+              <li>Photoshop (Anubhav, Chembur)</li>
+              <li>Coral Draw (Anubhav, Chembur)</li>
+            </ul>
+          </div>
+
+        </div>
+        <div class="col-9">
+          <div class="">
+            <div class="about-section">
+              <h3 style="letter-spacing: 3px;" class="mb-3">ABOUT ME</h3>
+              <p>A Software Developer, resilient, work oriented, possessing continuous evolving skills.<br>
+                I have 2 years of experience in programming in Python, Django, HTML, CSS, JavaScript.
+              </p>
+            </div>
+            <div class="divider my-4"></div>
+            <div class="about-section">
+              <h3 style="letter-spacing: 3px;" class="mb-3">CAREER OBJECTIVE</h3>
+              <p>Seeking a professional responsibility in Software development domain, handling different challenges to polish
+                my skills.
+              </p>
+            </div>
+            <div class="divider my-4"></div>
+            <div class="experience-section">
+              <h3 style="letter-spacing: 3px;" class="mb-3">WORK EXPERIENCE</h3>
+              <ul class="experiences main-ul">
+                <li class="my-3">
+                  <div class="experience-body">
+                    <p class="m-0"><i class="bi bi-calendar3 me-2 text-muted"></i>Sept 2021 - Present</p>
+                    <h3 class="m-0 mb-1 sub-heading">Software Developer</h3>
+                    <h6>Zeus Numerix, Pune</h6>
+                    <ul class="inner-ul">
+                      <li>
+                        <p class="m-0">Integrate Electronic components with software.</p>
+                      </li>
+                      <li>
+                        <p class="m-0">Work with Raspberry Pi.</p>
+                      </li>
+                      <li>
+                        <p class="m-0">Work with NetCDF data files.</p>
+                      </li>
+                      <li>
+                        <p class="m-0">Make customize plugin for wordpress application.</p>
+                      </li>
+                    </ul>
+                  </div>
+                </li>
+                <li class="my-3">
+                  <div class="experience-body">
+                    <p class="m-0"><i class="bi bi-calendar3 me-2 text-muted"></i>Jun 2021 - Sept 2021</p>
+                    <h3 class="m-0 mb-1 sub-heading">Graduate Trainee - Software Developer</h3>
+                    <h6>Zeus Numerix, Pune</h6>
+                    <ul class="inner-ul">
+                      <li>
+                        <p class="m-0">Install HPC clusters.</p>
+                      </li>
+                      <li>
+                        <p class="m-0">Work with linux bash scripts to automate work.</p>
+                      </li>
+                      <li>
+                        <p class="m-0">Work on cloud service like AWS ec2, AWS lightsall.</p>
+                      </li>
+                      <li>
+                        <p class="m-0">Create wireframes for GUI.</p>
+                      </li>
+                    </ul>
+                  </div>
+                </li>
+              </ul>
+            </div>
+            <div class="education-section mt-5">
+              <h3 style="letter-spacing: 3px;" class="mb-3">EDUCATION</h3>
+              <ul class="projects experiences main-ul">
+                <li class="my-4">
+                  <div class="experience-body">
+                    <p class="m-0"><i class="bi bi-calendar3 me-2 text-muted"></i>2018 - 2021</p>
+                    <h3 class="m-0 mb-1 sub-heading">B.Sc (Computer Science)</h3>
+                    <h6>Mumbai University</h6>
+                  </div>
+                </li>
+                <li class="my-4">
+                  <div class="experience-body">
+                    <p class="m-0"><i class="bi bi-calendar3 me-2 text-muted"></i>2016 - 2018</p>
+                    <h3 class="m-0 mb-1 sub-heading">H.S.C (Science)</h3>
+                    <h6>Maharashtra Board</h6>
+                  </div>
+                </li>
+              </ul>
+            </div>
+            <div class="page-break"></div>
+            <div class="projects mt-5">
+              <h3 style="letter-spacing: 3px;" class="mb-3 text-uppercase">Professional Projects</h3>
+              <ul class="projects experiences main-ul">
+                <li class="my-4">
+                  <div class="experience-body">
+                    <!-- <p class="m-0">Sept 2021 - Present</p> -->
+                    <h3 class="m-0 mb-1 sub-heading">GUI For Automatic Field Detection kit</h3>
+                    <div class="d-flex justify-content-between">
+                      <h6>Zeus Numerix, Pune</h6>
+                      <h6>Client: DRDE</h6>
+                    </div>
+                    <p class="m-0">
+                      We are making a kit for an DRDO lab, which can detect harmful substance from environment. For this
+                      Project I work as a GUI Developer, The User Interface I designed by using <b>Electron JS</b> and
+                      <b>vue JS</b>
+                      And backend server using <b>Python</b> and <b>Flask</b>.
+                    </p>
+                  </div>
+                </li>
+                <li class="my-4">
+                  <div class="experience-body">
+                    <!-- <p class="m-0">Sept 2021 - Present</p> -->
+                    <h3 class="m-0 mb-1 sub-heading">WEB Portal, for NCAP COALESCE</h3>
+                    <div class="d-flex justify-content-between">
+                      <h6>Zeus Numerix, Pune</h6>
+                      <h6>Client: IIT Bombay</h6>
+                    </div>
+                    <p class="m-0">
+                      We have built a web portal for NCAP COALESCE Project, To Storing, Visualize and Processing of
+                      Pollutant data.
+                      I work as a Web developer in this project, I built a Web Application Using <b>PHP</b>, and for
+                      Processing
+                      data I used <b>Python</b> as a backend language with <b>MySql</b> database. Also we handle NetCDF
+                      data with python
+                      <b>NetCDF4</b> library and <b>Pandas</b>.
+                    </p>
+                  </div>
+                </li>
+              </ul>
+            </div>
+            <div class="projects mt-5">
+              <h3 style="letter-spacing: 3px;" class="mb-3 text-uppercase">Personal Projects</h3>
+              <ul class="projects experiences">
+                <li class="my-4">
+                  <div class="experience-body">
+                    <!-- <p class="m-0">Sept 2021 - Present</p> -->
+                    <h3 class="m-0 mb-1 sub-heading">StockPlot</h3>
+                    <p class="m-0">
+                      StockPlot is a stock market analysis web application, which built to easily get information about
+                      stocks and track the trend of the stock market, making easy to invest in the stocks.
+                      I used <b>Django</b>, <b>Python</b> as backend language and <b>vue js</b> as frontend technology to built this app.
+                      <br>You can visit this app at <br>
+                      <a href="https://stockplot.anirudhajadhav.com/"
+                        target="_blank">https://stockplot.anirudhajadhav.com/</a>
+                    </p>
+                  </div>
+                </li>
+                <li class="my-4">
+                  <div class="experience-body">
+                    <!-- <p class="m-0">Sept 2021 - Present</p> -->
+                    <h3 class="m-0 mb-1 sub-heading">FitMySelf: A social media site for fitness freaks</h3>
+                    <p class="m-0">
+                      I designed and developed as social media web application, which mainly focus on fitness industry, i
+                      take the inspiration from facebook for its design.
+                      I used <b>Django</b> as backend server and
+                      <b>HTML</b>, <b>CSS</b> and
+                      <b>JavaScript</b> as frontend technology to built this app.
+                      <br> You can visit this app at<br>
+                      <a href="https://fitmyself.anirudhajadhav.com/" target="_blank"
+                        style="">https://fitmyself.anirudhajadhav.com/
+                      </a>
+                    </p>
+                  </div>
+                </li>
+              </ul>
+            </div>
+            <div class="achievements mt-5">
+              <h3 style="letter-spacing: 3px;" class="mb-3 text-uppercase">Achievements</h3>
+              <ul class="projects experiences">
+                <li class="my-4">
+                  <div class="experience-body">
+                    <!-- <p class="m-0">Sept 2021 - Present</p> -->
+                    <h3 class="m-0 mb-1 sub-heading">Debugging Competition</h3>
+                    <h5 class="">
+                      <i class="bi bi-trophy text-muted"></i> 2<sup>nd</sup> Rank
+                    </h5>
+                    <h5>
+                      <i class="bi bi-geo-alt-fill text-muted"></i>N.G. Acharya and D.K. Marathe College, Chembur, Mumbai
+                    </h5>
+                  </div>
+                </li>
+                <li class="my-4">
+                  <div class="experience-body">
+                    <!-- <p class="m-0">Sept 2021 - Present</p> -->
+                    <h3 class="m-0 mb-1 sub-heading">Sci Tech Utsav 2017</h3>
+                    <h5 class="">
+                      <i class="bi bi-trophy text-muted"></i> 2<sup>nd</sup> Rank
+                    </h5>
+                    <h5>
+                      <i class="bi bi-geo-alt-fill text-muted"></i> Shree L. R. Tiwari College Of Engineering, Thane.
+                    </h5>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </main>
+</template>
